598a43e40917d07a5e23cbf123dbeb9dc1df35b7
Vidit Kumar Singh <kumarvidit69@gmail.com>
2026-07-21 03:09:42 +0530

    Time: 0 ms (100%), Space: 42.4 MB (96.81%) - LeetHub

---
+++ 0232-implement-queue-using-stacks/0232-implement-queue-using-stacks.java
@@ -0,0 +1,38 @@
+import java.util.*;
+
+class MyQueue {
+
+    private Stack<Integer> inStack;
+    private Stack<Integer> outStack;
+
+    public MyQueue() {
+        inStack = new Stack<>();
+        outStack = new Stack<>();
+    }
+
+    public void push(int x) {
+        inStack.push(x);
+    }
+
+    public int pop() {
+        move();
+        return outStack.pop();
+    }
+
+    public int peek() {
+        move();
+        return outStack.peek();
+    }
+
+    public boolean empty() {
+        return inStack.isEmpty() && outStack.isEmpty();
+    }
+
+    private void move() {
+        if (outStack.isEmpty()) {
+            while (!inStack.isEmpty()) {
+                outStack.push(inStack.pop());
+            }
+        }
+    }
+}
