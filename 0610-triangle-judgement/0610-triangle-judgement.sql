8e12f35e9695c9d4a91c62270fe8497b42ed9fa2
Vidit Kumar Singh <kumarvidit69@gmail.com>
2026-09-15 13:41:10 +0530

    Time: 297 ms (79.62%), Space: 0B (100%) - LeetHub

---
+++ 0610-triangle-judgement/0610-triangle-judgement.sql
@@ -0,0 +1,9 @@
+# Write your MySQL query statement below
+select x,y,z,
+case 
+    when x+y<=z then 'No'
+    when y+z<=x then 'No'
+    when x+z<=y then 'No'
+    else 'Yes'
+end as triangle
+from triangle
